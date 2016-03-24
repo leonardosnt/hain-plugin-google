eae0c4aa4431234f0280d0a9e09fee29c3895e16
Leonardo Santos <devleeo@outlook.com>
2016-03-24 00:27:28 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = (pluginContext) => {
+  const shell = pluginContext.shell;
+
+  function search(query, res) {
+		const query_trim = query.trim();
+
+    if (query_trim.length == 0) {
+      return;
+    }
+
+    res.add({
+      id: query_trim,
+      payload: 'open',
+      title: query_trim,
+      desc: 'Search on Google.com'
+    });
+  }
+
+  function execute(id, payload) {
+    if (payload !== 'open') {
+      return;
+    }
+
+    shell.openExternal(`http://www.google.com/search?q=${id}`);
+  }
+
+  return { search, execute };
+};
